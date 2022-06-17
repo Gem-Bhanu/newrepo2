0a67593b6d24c5dd02d48aaa58092913dec8a6ca
Bhanu <bhanu.tyagi@geminisolutions.com>
2022-06-17 13:54:44 +0530

    initial

---
+++ src/main/java/com/example/practise2/repository/userRepository.java
@@ -0,0 +1,10 @@
+package com.example.practise2.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+
+public interface userRepository extends JpaRepository<user,Long> {
+}
